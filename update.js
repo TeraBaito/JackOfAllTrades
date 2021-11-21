928d1f1f1e0c03e9f80914b6a602212c32de6ab0
TeraBaito <lrobles2670@gmail.com>
2021-11-20 20:15:59 -0600

    Platform-wise newLines splitting

--- update.js
+++ update.js
@@ -17,9 +17,10 @@ ctx.fillStyle = '#ffffff';
 let xx = 50, yy = 90;
 
 (async function () {
+    const newLines = process.platform == 'win32' ? '\r\n' : '\n'; // fuck windows
     const playcounts = await require('./getPlaycounts')(token, '16775174');
     const statuses = (readFileSync('statusRaw.txt', { encoding: 'utf-8' }))
-        .split('\n') // IF USED IN WINDOWS, use \r\n instead
+        .split(newLines)
         .map((e, i) => {
             if (playcounts[i] > 5000) return { t: 'complete', c: '#35a200' };
             else if (e == '1') return { t: 'in progress', c: '#0000ff'};
